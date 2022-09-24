8f1e2c7423c01b5ee779ac0091d3ba3beea7de99
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2022-09-24 10:35:36 +0200

    add frames to keep layout management simpler

--- rgb-converter/main_window.py
+++ rgb-converter/main_window.py
@@ -7,7 +7,7 @@ Convert hex value to rgb an vice versa
 Version: 1.0
 Python 3.9+
 Date created: September 22th, 2022
-Date modified: -
+Date modified: September 24th, 2022
 """
 
 import tkinter as tk
@@ -28,7 +28,7 @@ class MainWindow:
         # self.window.iconbitmap('./assets/pythontutorial.ico')
 
         # Set width and height of the window
-        window_width = 700
+        window_width = 500
         window_height = 400
 
         # Get the screen dimension
@@ -45,63 +45,6 @@ class MainWindow:
         # Window is not resizable
         self.window.resizable(False, False)
 
-        # Configure the grid
-        self.window.columnconfigure(0, weight=1)
-        self.window.columnconfigure(1, weight=1)
-        self.window.columnconfigure(2, weight=1)
-        self.window.columnconfigure(3, weight=1)
-        self.window.columnconfigure(4, weight=1)
-
-        # Load logo image
-        logo_img = ImageTk.PhotoImage(file="assets/rgb-converter-logo.png")
-        logo_widget = ttk.Label(self.window, image=logo_img)
-        logo_widget.image = logo_img
-        logo_widget.grid(columnspan=5, column=0, row=0)
-
-        # Load labels
-        hex_label = ttk.Label(self.window,
-                              text="Enter hex value:",
-                              font=("Helvetica", 16))
-        hex_label.grid(column=0, row=1, pady=4, sticky="w")
-
-        rgb_label = ttk.Label(self.window,
-                              text="Enter RGB values:",
-                              font=("Helvetica", 16))
-        rgb_label.grid(column=0, row=2, pady=4, sticky="w")
-
-        output_label = ttk.Label(self.window,
-                                 text="Result:",
-                                 font=("Helvetica", 16))
-        output_label.grid(column=0, row=3, pady=15, sticky="w")
-
-        result_label = ttk.Label(self.window,
-                                 text="",
-                                 font=("Helvetica", 16))
-        result_label.grid(column=1, columnspan=2, row=3, pady=15, sticky="w")
-
-        # Load entries
-        hex_text = tk.StringVar()
-        hex_textbox = ttk.Entry(self.window, textvariable=hex_text, width=6)
-        hex_textbox.grid(column=1, row=1, sticky="w")
-
-        r_text = tk.StringVar()
-        r_textbox = ttk.Entry(self.window, textvariable=r_text, width=4)
-        r_textbox.grid(column=1, row=2, sticky="w")
-
-        g_text = tk.StringVar()
-        g_textbox = ttk.Entry(self.window, textvariable=g_text, width=4)
-        g_textbox.grid(column=2, row=2, sticky="w")
-
-        b_text = tk.StringVar()
-        b_textbox = ttk.Entry(self.window, textvariable=b_text, width=4)
-        b_textbox.grid(column=3, row=2, sticky="w")
-
-        exit_button = tk.Button(self.window,
-                                text="Exit",
-                                width=6,
-                                command=self.window.destroy)
-        exit_button.grid(column=0, row=4, sticky="w")
-
         def _show_rgb_values():
             user_input = hex_text.get()
             result = converter.hex_to_rgb(user_input)
@@ -115,8 +58,36 @@ class MainWindow:
             result = converter.rgb_to_hex(r_input, g_input, b_input)
             result_label.config(text=result)
 
-        # Load buttons
-        hex_button = tk.Button(self.window,
+        # Load logo image
+        logo_img = ImageTk.PhotoImage(file="assets/rgb-converter-logo.png")
+        logo_widget = ttk.Label(self.window, image=logo_img)
+        logo_widget.image = logo_img
+        logo_widget.grid()
+
+        # Create Widgets in a Frame (mf = main frame)
+        # to keep geometry management simpler
+        mf = ttk.Frame(self.window)
+        mf.grid(padx=20, sticky=(tk.E + tk.W))
+        mf.columnconfigure(0, weight=1)
+
+        # Input Frame (Hex & RGB values)
+        input_frame = ttk.LabelFrame(mf, text="Input")
+        input_frame.grid(sticky=(tk.W + tk.E))
+        for i in range(2):
+            input_frame.columnconfigure(i, weight=1)
+
+        # Hex Label & Entry (row 1)
+        hex_label = ttk.Label(input_frame,
+                              text="Enter hex value:",
+                              font=("Helvetica", 14))
+        hex_label.grid(column=0, row=1, sticky=tk.W)
+
+        hex_text = tk.StringVar()
+        hex_textbox = ttk.Entry(input_frame, textvariable=hex_text, width=6)
+        hex_textbox.grid(column=1, row=1, sticky=(tk.W + tk.E))
+
+        # Hex button
+        hex_button = tk.Button(input_frame,
                                text="Show RGB values",
                                height=1,
                                width=10,
@@ -124,13 +95,54 @@ class MainWindow:
                                )
         hex_button.grid(column=4, row=1)
 
-        rgb_button = tk.Button(self.window,
+        # RGB Labels & Entries (row 2)
+        rgb_label = ttk.Label(input_frame,
+                              text="Enter RGB values:",
+                              font=("Helvetica", 14))
+        rgb_label.grid(column=0, row=2, sticky=tk.W)
+
+        r_text = tk.StringVar()
+        r_textbox = ttk.Entry(input_frame, textvariable=r_text, width=6)
+        r_textbox.grid(column=1, row=2, sticky=(tk.W + tk.E))
+
+        g_text = tk.StringVar()
+        g_textbox = ttk.Entry(input_frame, textvariable=g_text, width=6)
+        g_textbox.grid(column=2, row=2, sticky=(tk.W + tk.E))
+
+        b_text = tk.StringVar()
+        b_textbox = ttk.Entry(input_frame, textvariable=b_text, width=6)
+        b_textbox.grid(column=3, row=2, sticky=(tk.W + tk.E))
+
+        # RGB button
+        rgb_button = tk.Button(input_frame,
                                text="Show hex value",
                                height=1,
                                width=10,
                                command=_show_hex_value
                                )
         rgb_button.grid(column=4, row=2)
+
+        # Output Frame
+        output_frame = ttk.LabelFrame(mf, text="Output")
+        output_frame.grid(pady=40, sticky=(tk.W + tk.E))
+        for i in range(2):
+            input_frame.columnconfigure(i, weight=1)
+
+        output_label = ttk.Label(output_frame,
+                                 text="Result:",
+                                 font=("Helvetica", 14))
+        output_label.grid(column=0, row=1, pady=15, sticky="w")
+
+        result_label = ttk.Label(output_frame,
+                                 text="",
+                                 font=("Helvetica", 14))
+        result_label.grid(column=1, row=1, sticky="w")
+
+        exit_button = tk.Button(mf,
+                                text="Exit",
+                                width=6,
+                                command=self.window.destroy)
+        exit_button.grid(sticky=tk.E)
 
     def mainloop(self):
         self.window.mainloop()
