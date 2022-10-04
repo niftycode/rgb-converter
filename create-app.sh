146c9eee74309b441de67ffb085e727b7d2baa9e
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2022-10-04 18:02:31 +0200

    update gitignore

--- create-app.sh
+++ create-app.sh
@@ -3,6 +3,8 @@
 rm -rfv build
 rm -rfv dist
 pyinstaller main.spec
+# pyinstaller --onefile --windowed --icon icon.icns --name testApp main.py
+# pyinstaller -w --debug=all --onefile --windowed --name RGB-Converter rgb_converter/main_window.py
 
 cd dist
 open .
